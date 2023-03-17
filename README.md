3ecdc4504eb81b0cbab484b4453954e9f341ef5f
zanelf <z4n.3lf@gmail.com>
2023-03-16 22:16:28 -0300

    0.0.4

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ para clonar un repositorio (solo tendras que hacerlo una vez)
 - ve a la direccion donde quieres poner el proyecto 
 - escribe en el cmd ```git clone <url del proyecto> ```
 
-### paso 2 (optativo): creando una rama  
+### paso 2 (optativo): creando una rama o moviendote a otra
 
-- 
+una gracia de git como puede que sepas es el tema de las ramas y versiones, este es un paso optativo en caso de que quieras estar mas segura que con tus cambios no "romperas" nada. 
+
+asi que aqui asumiremos que quieres crear una rama para ti solita 
+    
+- para crear una rama 
--- recursos/scripts/contador.js
+++ recursos/scripts/contador.js
@@ -1,24 +1,11 @@
 var a = 45646456;
 
-//var  texto = document.getElementById("prueba-contador")
+//var  texto = document.getElementById("prueba-contador")   
 
-
-var boton = function() {
-    a = a-1;
-    $("#prueba-contador").text(a);
-}
-
-var boton2 = () => {
-    a = a-2;
-    $("#prueba-contador").text(a);
-}
-
-$("#botonTexto").click(() => {
-    
+$("#botonTexto").click(() => {    
     a = a-1;
     $("#prueba-contador").text(a);
 })
-
 
 let contador = 0
 var fuentes = ["Helvetica","Arial" ,"Arial Black" ,"Verdana" ,"Tahoma" ,"Trebuchet MS" ,"Impact" ,"Gill Sans" ,"Times New Roman","Georgia","Palatino" ,"Baskerville" ,"Andalé Mono","Courier","Lucida","Monaco","Bradley Hand","Brush Script MT" ,"Luminari","Comic Sans MS" ]
